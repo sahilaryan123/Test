e4fc3203372f39b3f8ec595f9872ba10934b4d94
Sohil Aryan <sohilaryan@Sohils-MacBook-Air.local>
2020-02-06 12:01:51 -0500

    first commit

---
+++ src/com/test/TestHello.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestHello {
+
+	@Test
+	public static void mainn() {
+
+		System.out.println("Hello World");
+	    System.out.println("hows everything going");
+	}
+
+}
